4104a79b53c454f85162b14abcc7762c7f6342a2
Rahi Smaga <Rahi Smaga>
2016-10-11 00:12:06 +0200

    1st commit of the project ^^

---
+++ src/test/java/com/javacodegeeks/testng/maven/NewTest.java
@@ -0,0 +1,10 @@
+package com.javacodegeeks.testng.maven;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+}
